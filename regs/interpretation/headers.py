1975fe264e74e6dbbe9051fb97474efe4807db16
CM Lubinski <cm.lubinski@gmail.com>
2013-03-20 00:50:08 +0100

    Interpretation parser. Does not have full test coverage yet

--- regs/interpretation/headers.py
+++ regs/interpretation/headers.py
@@ -1,5 +1,6 @@
-import string
 from pyparsing import Word, Optional, LineStart, LineEnd, SkipTo, Literal
+import re
+import string
 
 lower_alpha_sub = "(" + Word(string.ascii_lowercase).setResultsName("id") + ")"
 upper_alpha_sub = "(" + Word(string.ascii_uppercase).setResultsName("id") + ")"
@@ -19,3 +20,6 @@ def parse(plain_text, section):
     header = whole_par.setResultsName("whole") | keyterm.setResultsName("keyterm")
 
     return list(header.scanString(plain_text))
+
+def get_section(title, part):
+    return re.match(r'^Section %d.(\d+)(.*)$' % part, title).group(1)
