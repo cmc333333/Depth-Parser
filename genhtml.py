955e4997779d909844e1171bd7c31c4acc648e13
CM Lubinski <cm.lubinski@gmail.com>
2013-03-27 16:49:53 +0100

    Generate HTML uses the new interpretation layer

--- genhtml.py
+++ genhtml.py
@@ -1,27 +1,35 @@
 import codecs
 import re
-from regs.depth import build_regulation_tree
-from regs.interpretation.layer import build
+from regs.depth import build_regulation_tree, tree
+from regs.depth.supplement import find_supplement_start
+from regs.depth.interpretation.tree import build as build_interp_tree
+from regs.layers import interpretation
 from regs.terms import build_layer
 from xml.sax.saxutils import quoteattr
 
 f = codecs.open('rege.txt', encoding='utf-8')
 rege = unicode(f.read())
 f.close()
-f = codecs.open('interp.txt', encoding='utf-8')
-interp = unicode(f.read())
-f.close()
 
-tree = build_regulation_tree(rege, 1005)
-interp_layer = build(interp, 1005)
-term_layer = build_layer(tree)
+interp = rege[find_supplement_start(rege):]
+
+rege_tree = build_regulation_tree(rege, 1005)
+interp_tree = build_interp_tree(interp, 1005)
+interp_layer = interpretation.build(interp_tree, 1005)
+term_layer = build_layer(rege_tree)
+
+import json 
+print json.dumps(interp_tree['children'][14]['children'][1]['children'])
 
 indexed_reg = {}
 def index_tree(tree):
     indexed_reg[tree['label']['text']] = tree
-    for child in tree['children']:
-        index_tree(child)
-index_tree(tree)
+tree.walk(rege_tree, index_tree)
+
+indexed_interp = {}
+def index_tree(tree):
+    indexed_interp[tree['label']['text']] = tree
+tree.walk(interp_tree, index_tree)
 
 def as_text(tree):
     text = tree['text']
@@ -51,7 +59,7 @@ def print_node(node, relevant_terms = None):
 
     if lab in interp_layer and 'interpretation' in interp_layer[lab]:
         doc.write("<a title=__TITLE__%d" % len(titles) + " href='#not-done'>?</a>")
-        titles.append(quoteattr(interp_layer[lab]['interpretation'].strip()))
+        titles.append(quoteattr(as_text(indexed_interp[interp_layer[lab]['interpretation']]).strip()))
     if 'title' in node['label']:
         doc.write('<h2>')
         doc.write(node['label']['title'])
@@ -63,7 +71,7 @@ def print_node(node, relevant_terms = None):
         for term, interp in interp_layer[lab]['keyterms']:
             text = re.sub(r'(?i)\b' + term + r'\b', term + "<a title=__TITLE__%d href='#not-done'>" % len(titles) + 
                     "?</a>", text)
-            titles.append(quoteattr(interp.strip()))
+            titles.append(quoteattr(as_text(indexed_interp[interp]).strip()))
     #   definitions
     for term in sorted_terms:
         text = re.sub(r'(?i)\b' + term + r'\b', 
@@ -79,7 +87,7 @@ def print_node(node, relevant_terms = None):
             doc.write('</li>')
         doc.write('</ol>')
 
-print_node(tree)
+print_node(rege_tree)
 
 txt = doc.getvalue()
 for i in range(len(titles)-1, -1, -1):
